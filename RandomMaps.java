9ee853523fce48d94049f8c350e4d5e9aee53de0
Jens Hofschröer <git@nigjo.de>
2022-03-01 13:49:14 +0100

    refactoring of the class. - logging to stderr, properties-echo to stdout. - added "--help"

--- RandomMaps.java
+++ RandomMaps.java
@@ -1,15 +1,14 @@
+package de.nigjo.wreckfest.evtloop;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Properties;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 
 //
@@ -36,6 +35,8 @@ import java.util.stream.Collectors;
  */
 public class RandomMaps
 {
+  private static final int LOG_LINE_LENGTH = 30;
+
   static class Track
   {
     static String lastName;
@@ -66,47 +67,197 @@ public class RandomMaps
     }
   }
 
-  public static void main(String... args) throws Exception
+  /*"bots", "ai_difficulty", */
+  static List<String> trackSettingKeys = Arrays.asList(
+      "disabled",
+      "gamemode", "num_teams", "laps", "time_limit",
+      "elimination_interval", "vehicle_damage", "car_class_restriction",
+      "car_restriction", "special_vehicles_disabled", "car_reset_disabled",
+      "car_reset_delay", "wrong_way_limiter_disabled", "weather");
+  static List<String> raceOnlyKeys = Arrays.asList(
+      "num_teams", "laps", "elimination_interval",
+      "wrong_way_limiter_disabled");
+  static List<String> derbyOnlyKeys = Arrays.asList(
+      "time_limit");
+
+  private static void log(String pattern, Object... args)
   {
-    String filebase = RandomMaps.class.getSimpleName();
-    Files.copy(
-        Path.of(filebase + "_base.cfg"),
-        Path.of(filebase + ".cfg"),
-        StandardCopyOption.REPLACE_EXISTING
-    );
-
-    URL resource = RandomMaps.class.getResource(filebase + ".properties");
-    System.out.println("resource=" + resource);
-
-    Properties settings = new Properties();
-    if(Files.exists(Path.of(filebase + ".properties")))
+    if(args.length == 0)
     {
-      try(InputStream in = Files.newInputStream(Path.of(filebase + ".properties")))
+      System.err.println(pattern);
+    }
+    else
+    {
+      System.err.printf(pattern + "%n", args);
+    }
+  }
+
+  private static Properties loadProperties(String filename) throws IOException
+  {
+    Properties settings = new Properties();
+    if(Files.exists(Path.of(filename)))
+    {
+      try( InputStream in = Files.newInputStream(Path.of(filename)))
       {
         settings.load(in);
       }
     }
+    return settings;
+  }
 
-    var tracks =
-        Files.lines(Path.of(filebase + "_tracks.tsv"), StandardCharsets.UTF_8)
+  private static final Map<String, String> HELP = Map.of(
+      "help", "this help.",
+      "output", "filename of the output config file. Defaults to 'RandomMaps.cfg'.",
+      "base", "filename of the base config file without an event loop."
+      + " Is copied to the output before the event loop."
+      + " Default is 'RandomMaps_base.cfg'",
+      "maps", "Tab separated file with track list."
+      + " Default is 'RandomMaps.tsv'.",
+      "settings", "settings file with settings per track."
+      + " Default is 'RandomMaps.properties'"
+  );
+
+  private static boolean parseArgs(Map<String, String> config, String... args)
+      throws Exception
+  {
+    config.putIfAbsent("help", "false");
+    String option = null;
+    for(String arg : args)
+    {
+      if(option != null)
+      {
+        config.put(option, arg);
+        option = null;
+      }
+      else if(arg.startsWith("--"))
+      {
+        option = arg.substring(2);
+        if(!config.containsKey(option))
+        {
+          log("Unknown option \"%s\"", arg);
+          return false;
+        }
+        switch(option)
+        {
+          case "help":
+            System.err.println("java RandomMaps.java [<options>]");
+            System.err.println();
+            System.err.println("<options>:");
+            for(String optionKey : new TreeSet<>(config.keySet()))
+            {
+              System.err.print("--" + optionKey);
+              if(HELP.containsKey(optionKey))
+              {
+                System.err.print("  ");
+                System.err.print(HELP.get(optionKey));
+              }
+              System.err.println();
+            }
+            return false;
+        }
+      }
+      else
+      {
+        log("Unknown argument \"%s\"", arg);
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public static void main(String... args) throws Exception
+  {
+    Map<String, String> config = new HashMap<>();
+    config.put("settings", "RandomMaps.properties");
+    config.put("maps", "RandomMaps_tracks.tsv");
+    config.put("base", "RandomMaps_base.cfg");
+    config.put("output", "RandomMaps.cfg");
+
+    if(!parseArgs(config, args))
+    {
+      return;
+    }
+
+    //String filebase = RandomMaps.class.getSimpleName();
+    Files.copy(
+        Path.of(config.get("base")),
+        Path.of(config.get("output")),
+        StandardCopyOption.REPLACE_EXISTING
+    );
+
+    List<Track> tracks = loadTracks(config.get("maps"));
+    log("Tracks found: %d", tracks.size());
+
+    log("Event loop:");
+    log("-".repeat(LOG_LINE_LENGTH));
+
+    Map<String, Map<String, String>> tracksettings =
+        createEventLoop(tracks, config.get("settings"), config.get("output"));
+
+    log("-".repeat(LOG_LINE_LENGTH));
+    log("properties:");
+    printToolProperties(tracksettings);
+    log("-".repeat(LOG_LINE_LENGTH));
+  }
+
+  private static List<Track> loadTracks(String tracksfile) throws IOException
+  {
+    var alltracks =
+        Files.lines(Path.of(tracksfile), StandardCharsets.UTF_8)
             .filter(line -> !line.startsWith("#"))
             .map(Track::of)
-            .filter(t -> !Boolean.parseBoolean(settings.getProperty(t.id + ".disabled")))
             .collect(Collectors.toList());
-    System.out.println("Tracks found: " + tracks.size());
+    var tracks = alltracks.stream()
+        //.filter(t -> !Boolean.parseBoolean(settings.getProperty(t.id + ".disabled")))
+        .collect(Collectors.toList());
+    return tracks;
+  }
 
+  private static void printToolProperties(
+      Map<String, Map<String, String>> tracksettings)
+  {
+    tracksettings.forEach((keyname, data) ->
+    {
+      String[] info = keyname.split("\\|");
+      System.out.println("## \"" + info[0] + "\" - " + info[1]);
+      String id = info[2];
+      for(Map.Entry<String, String> e : data.entrySet())
+      {
+        String key = e.getKey();
+        if(key.charAt(0) == '#')
+        {
+          key = '#' + id + '.' + key.substring(1);
+        }
+        else
+        {
+          key = id + '.' + key;
+        }
+        System.out.println(key + "=" + e.getValue());
+      }
+    });
+  }
+
+  private static Map<String, Map<String, String>> createEventLoop(
+      List<Track> tracks, String settingsFile, String outputFile)
+      throws IOException
+  {
+    Properties serverSettings = loadProperties(outputFile);
+    serverSettings.putIfAbsent("disabled", "true");
+
+    Properties toolSettings = loadProperties(settingsFile);
+
+    Map<String, Map<String, String>> tracksettings = new TreeMap<>();
     Random rnd = new Random(System.currentTimeMillis());
     int teamModeThreshold = Integer.parseInt(
-        settings.getProperty(filebase + ".teamModeThreshold", "1"));
+        toolSettings.getProperty("RandomMaps.teamModeThreshold", "1"));
     int deathmatchThreshold = Integer.parseInt(
-        settings.getProperty(filebase + ".deathmatchThreshold", "1"));
+        toolSettings.getProperty("RandomMaps.deathmatchThreshold", "1"));
 
-    System.out.println("Event loop:");
-    System.out.println("------------------------------");
     var lastMaps = new ArrayList<String>();
     String lastKind = null;
-    try(BufferedWriter out = Files.newBufferedWriter(
-        Path.of(filebase + ".cfg"), StandardOpenOption.APPEND))
+    try( BufferedWriter out = Files.newBufferedWriter(
+        Path.of(outputFile), StandardOpenOption.APPEND))
     {
       while(!tracks.isEmpty())
       {
@@ -124,6 +275,25 @@ public class RandomMaps
         }
         while(--scancounter > 0);
         tracks.remove(index);
+
+        Properties trackdata = new Properties(serverSettings);
+        if(Boolean.parseBoolean(toolSettings.getProperty(next.id + ".disabled")))
+        {
+          log("-- skipping \"%s\" - %s", next.name, next.kind);
+          trackdata.setProperty("disabled", "true");
+          if(next.racetype.isEmpty())
+          {
+            trackdata.setProperty("gamemode", "derby");
+          }
+          else
+          {
+            trackdata.setProperty("gamemode", "racing");
+          }
+          copyMapdataFromGlobal(next, toolSettings, trackdata);
+          addTrackProperties(trackdata, tracksettings, next);
+          continue;
+        }
+
         lastKind = next.kind;
         if(lastMaps.size() > 4)
         {
@@ -136,7 +306,6 @@ public class RandomMaps
             .append(next.id);
         out.newLine();
 
-        Properties trackdata = new Properties();
         //set "default" mode
         if(next.racetype.isEmpty())
         {
@@ -148,24 +317,24 @@ public class RandomMaps
           {
             trackdata.setProperty("gamemode", "derby deathmatch");
           }
-        }
-        else if(rnd.nextInt(100) < teamModeThreshold)
-        {
-          trackdata.setProperty("gamemode", "team race");
+          trackdata.setProperty("special_vehicles_disabled", "0");
         }
         else
         {
-          trackdata.setProperty("gamemode", "racing");
+          if(rnd.nextInt(100) < teamModeThreshold)
+          {
+            trackdata.setProperty("gamemode", "team race");
+            //trackdata.setProperty("num_teams", trackdata.getProperty("num_teams"));
+          }
+          else
+          {
+            trackdata.setProperty("gamemode", "racing");
+          }
+          trackdata.setProperty("special_vehicles_disabled",
+              trackdata.getProperty("special_vehicles_disabled"));
         }
-        String trackId = next.id;
-        settings.stringPropertyNames().stream()
-            .filter(key -> key.startsWith(trackId + '.'))
-            .collect(
-                () -> trackdata,
-                (p, key) -> p.setProperty(
-                    key.substring(key.indexOf('.') + 1), settings.getProperty(key)),
-                (p1, p2) -> p2.clear());
-        //System.out.println(trackdata);
+
+        copyMapdataFromGlobal(next, toolSettings, trackdata);
 
         trackdata.remove("disabled");
 
@@ -176,10 +345,53 @@ public class RandomMaps
           out.newLine();
         }
 
-        System.out.println(next.name + " - " + next.kind
-            + " (" + trackdata.getProperty("gamemode") + ")");
+        addTrackProperties(trackdata, tracksettings, next);
+
+        log("-- \"%s\" - %s (%s)",
+            next.name, next.kind, trackdata.getProperty("gamemode"));
       }
     }
-    System.out.println("------------------------------");
+
+    return tracksettings;
+  }
+
+  private static void copyMapdataFromGlobal(Track next, Properties settings,
+      Properties trackdata)
+  {
+    String trackId = next.id;
+    settings.stringPropertyNames().stream()
+        .filter(key -> key.startsWith(trackId + '.'))
+        .collect(
+            () -> trackdata,
+            (p, key) -> p.setProperty(
+                key.substring(key.indexOf('.') + 1), settings.getProperty(key)),
+            (p1, p2) -> p2.clear());
+  }
+
+  private static void addTrackProperties(Properties trackdata,
+      Map<String, Map<String, String>> tracksettings, Track next)
+  {
+    Map<String, String> mapProperties = new LinkedHashMap<>();
+    trackSettingKeys.forEach(
+        key -> mapProperties.put(
+            (trackdata.containsKey(key) ? "" : "#") + key,
+            trackdata.getProperty(key))
+    );
+    switch(trackdata.getProperty("gamemode"))
+    {
+      case "racing":
+      case "team race":
+        derbyOnlyKeys.forEach(key -> mapProperties.remove(key));
+        derbyOnlyKeys.forEach(key -> mapProperties.remove('#' + key));
+        break;
+      case "derby deathmatch":
+      case "derby":
+        raceOnlyKeys.forEach(key -> mapProperties.remove(key));
+        raceOnlyKeys.forEach(key -> mapProperties.remove('#' + key));
+        break;
+      default:
+        log("unknown gamemode: %s", trackdata.getProperty("gamemode"));
+    }
+    tracksettings.put(next.name + "|" + next.kind + "|" + next.id, mapProperties);
   }
 }
